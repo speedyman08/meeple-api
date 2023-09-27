23a6776bc64488860ba2b5a9d412e6e4214fee03
Stephen <stephen.jr@selyuk.com>
2023-09-27 20:01:03 +0300

    Make DB url an environ variable

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-
+import { configDotenv } from "dotenv";
+configDotenv()
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/database');
+    await mongoose.connect(process.env.DB_URL as string);
     console.log("DB connected");
   } catch (err) {
     console.error("Failed to connect to MongoDB:", err);
